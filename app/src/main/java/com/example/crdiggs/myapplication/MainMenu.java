a06b930e668c444058f57d3b29110e5c4f6ade60
Justin Koo <bjustinkoo@gmail.com>
2018-02-26 22:52:58 -0600

    Added 'start plan' button to main menu

--- app/src/main/java/com/example/crdiggs/myapplication/MainMenu.java
+++ app/src/main/java/com/example/crdiggs/myapplication/MainMenu.java
@@ -1,7 +1,10 @@
 package com.example.crdiggs.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        Button signInButton = findViewById(R.id.start_plan_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent teacherInfoIntent = new Intent(MainMenu.this, TeacherInfo.class);
+                startActivity(teacherInfoIntent);
+            }
+        });
     }
 }
