164610055b40248505f2862586e69e6c0b543ead
Justin Koo <bjustinkoo@gmail.com>
2018-04-17 16:36:44 -0500

    Left only 5 competencies to choose from. Added instruction text to competency activity. Added continue button to comptency activity

--- app/src/main/java/com/example/crdiggs/myapplication/Competencies.java
+++ app/src/main/java/com/example/crdiggs/myapplication/Competencies.java
@@ -1,7 +1,10 @@
 package com.example.crdiggs.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Competencies extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Competencies extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_competencies);
+
+        Button competenciesContinueButton = findViewById(R.id.competencies_continue_button);
+        competenciesContinueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent subcompetenciesIntent = new Intent(Competencies.this, Subcompetencies.class);
+                startActivity(subcompetenciesIntent);
+            }
+        });
     }
 }
