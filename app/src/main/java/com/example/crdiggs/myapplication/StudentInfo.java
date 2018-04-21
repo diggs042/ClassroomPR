23fdf27eb12c396fb95e5562969678aff9a56fd2
Jared Lim <jaredlim320@gmail.com>
2018-04-21 14:52:58 -0500

    reworking student info ui

--- app/src/main/java/com/example/crdiggs/myapplication/StudentInfo.java
+++ app/src/main/java/com/example/crdiggs/myapplication/StudentInfo.java
@@ -1,5 +1,6 @@
 package com.example.crdiggs.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,17 +14,13 @@ public class StudentInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_info);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Intent intent = getIntent();
+        int studentCount = intent.getExtras().getInt("studentCount");
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+
+        // design own mini xml file to contain 1 student entry, then use that ui element (fragment)
+        // just add stuff
     }
 
 }
--- app/src/main/java/com/example/crdiggs/myapplication/TeacherInfo.java
+++ app/src/main/java/com/example/crdiggs/myapplication/TeacherInfo.java
@@ -15,29 +15,31 @@ public class TeacherInfo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_info);
 
-<<<<<<< HEAD
+
         final EditText studentCount = findViewById(R.id.number_students_input);
-=======
+
         // Will remove later
         final Teacher teacher = FirebaseHandler.generateMainTeacher();
 
->>>>>>> 5ebc27e32d9505dcb750429b51409348e7441d49
+
         Button continueButton = findViewById(R.id.student_names_button);
         continueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 Intent studentInfoIntent = new Intent(TeacherInfo.this, StudentInfo.class);
-<<<<<<< HEAD
-                String numStudents = studentCount.getText().toString();
-                studentInfoIntent.putExtra("numStudents", numStudents);
-                Log.wtf("msg", numStudents);
-=======
+
+
 
                 String name = ((EditText) findViewById(R.id.teacher_name_input)).getText().toString();
                 String schoolName = ((EditText) findViewById(R.id.school_name_input)).getText().toString();
                 int grade = Integer.parseInt(((EditText) findViewById(R.id.grade_input)).getText().toString());
                 int studentCount = Integer.parseInt(((EditText) findViewById(R.id.number_students_input)).getText().toString());
+
+                // String studentCount = studentCount.getText().toString();
+                studentInfoIntent.putExtra("studentCount", studentCount);
+                Log.wtf("msg", Integer.toString(studentCount));
+
 
                 if(teacher != null) {
                     teacher.setTeacherName(name);
@@ -50,7 +52,7 @@ public class TeacherInfo extends AppCompatActivity {
                     Log.d("NullTeacher", "There's a headless teacher running");
                 }
 
->>>>>>> 5ebc27e32d9505dcb750429b51409348e7441d49
+
                 startActivity(studentInfoIntent);
 
             }
