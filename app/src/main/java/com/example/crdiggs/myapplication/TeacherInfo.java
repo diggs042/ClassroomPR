4e1b4fac365fd27c24e3534fbf472619b159e238
Jared Lim <jaredlim320@gmail.com>
2018-04-21 13:50:28 -0500

    teacher info

--- app/src/main/java/com/example/crdiggs/myapplication/TeacherInfo.java
+++ app/src/main/java/com/example/crdiggs/myapplication/TeacherInfo.java
@@ -3,8 +3,10 @@ package com.example.crdiggs.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class TeacherInfo extends AppCompatActivity {
 
@@ -13,14 +15,19 @@ public class TeacherInfo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_info);
 
+        final EditText studentCount = findViewById(R.id.number_students_input);
         Button continueButton = findViewById(R.id.student_names_button);
         continueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent studentInfoIntent = new Intent(TeacherInfo.this, StudentInfo.class);
+                String numStudents = studentCount.getText().toString();
+                studentInfoIntent.putExtra("numStudents", numStudents);
+                Log.wtf("msg", numStudents);
                 startActivity(studentInfoIntent);
             }
         });
+
     }
 
 }
