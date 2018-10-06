701b47d7ebf7d4f2a13c8ee00c93d60d8090d358
Justin Koo <bjustinkoo@gmail.com>
2018-10-06 00:18:25 -0500

    Added StudentsAndCompetenciesActivity

---
+++ app/src/main/java/com/example/crdiggs/myapplication/Activities/StudentsAndCompetenciesActivity.java
@@ -0,0 +1,15 @@
+package com.example.crdiggs.myapplication.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.crdiggs.myapplication.R;
+
+public class StudentsAndCompetenciesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_students_and_competencies);
+    }
+}
